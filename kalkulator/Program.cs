d002b0b264e3d9e3ded0d2723485b551ab583d86
isajaisyullah <isa.jaisyullah@gmail.com>
2023-11-16 11:44:52 +0700

    Add project files.

---
+++ kalkulator/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Menampilkan pesan nama program
+            Console.WriteLine("Kalkulator Sederhana");
+
+            // Memulai loop input user
+            while (true)
+            {
+                // User input
+                Console.Write("\nMasukkan ekspresi (contoh, 5 + 3, 10 * 2, 8 / 4): ");
+                string input = Console.ReadLine();
+
+                // check user ingin keluar dari program
+                if (input.ToLower() == "exit")
+                {
+                    break; // Keluar dari loop jika pengguna mengetik "exit"
+                }
+
+                try
+                {
+                    // Menilai input user dan menampilkan hasil
+                    double result = EvaluateExpression(input);
+                    Console.WriteLine();
+                    Console.WriteLine($"Hasil: {result}");
+                    Console.WriteLine("-------------------------------------------------------");
+                }
+                catch (Exception ex)
+                {
+                    // catch error
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+        }
+
+        public static double EvaluateExpression(string expression)
+        {
+            // Membagi input dengan batasan spasi
+            string[] elements = expression.Split(' ');
+
+            // Memeriksa apakah ekspresi memiliki format yang benar (operand1 operator operand2)
+            if (elements.Length != 3)
+            {
+                throw new ArgumentException("Format ekspresi tidak valid. Gunakan format: operand1 operator operand2");
+            }
+
+            // Mem-parsing operand dan operator dari ekspresi
+            double operand1 = double.Parse(elements[0]);
+            double operand2 = double.Parse(elements[2]);
+            string operation = elements[1];
+
+            // Melakukan operasi aritmatika yang sesuai berdasarkan operator
+            switch (operation)
+            {
+                case "+":
+                    return operand1 + operand2;
+                case "-":
+                    return operand1 - operand2;
+                case "*":
+                    return operand1 * operand2;
+                case "/":
+                    // Memeriksa pembagian dengan nol
+                    if (operand2 == 0)
+                    {
+                        throw new DivideByZeroException("Tidak dapat dibagi dengan nol.");
+                    }
+                    return operand1 / operand2;
+                default:
+                    // Melempar pengecualian untuk operator yang tidak valid
+                    throw new ArgumentException($"Operator tidak valid: {operation}");
+            }
+        }
+    }
+}
---
+++ kalkulatorTests/ProgramTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using kalkulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kalkulator.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void EvaluateExpression_Addition()
+        {
+            // Pengaturan kondisi awal (Arrange)
+            string expression = "5 + 3";
+
+            // Melakukan tindakan (Act)
+            double result = Program.EvaluateExpression(expression);
+
+            // Memastikan hasil sesuai dengan yang diharapkan (Assert)
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod()]
+        public void EvaluateExpression_Subtraction()
+        {
+            // Pengaturan kondisi awal (Arrange)
+            string expression = "10 - 2";
+
+            // Melakukan tindakan (Act)
+            double result = Program.EvaluateExpression(expression);
+
+            // Memastikan hasil sesuai dengan yang diharapkan (Assert)
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod()]
+        public void EvaluateExpression_Multiplication()
+        {
+            // Pengaturan kondisi awal (Arrange)
+            string expression = "4 * 3";
+
+            // Melakukan tindakan (Act)
+            double result = Program.EvaluateExpression(expression);
+
+            // Memastikan hasil sesuai dengan yang diharapkan (Assert)
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod()]
+        public void EvaluateExpression_Division()
+        {
+            // Pengaturan kondisi awal (Arrange)
+            string expression = "16 / 4";
+
+            // Melakukan tindakan (Act)
+            double result = Program.EvaluateExpression(expression);
+
+            // Memastikan hasil sesuai dengan yang diharapkan (Assert)
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void EvaluateExpression_DivisionByZero()
+        {
+            // Pengaturan kondisi awal (Arrange)
+            string expression = "10 / 0";
+
+            // Melakukan tindakan (Act)
+            double result = Program.EvaluateExpression(expression);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EvaluateExpression_InvalidExpression()
+        {
+            // Pengaturan kondisi awal (Arrange)
+            string expression = "invalid expression";
+
+            // Melakukan tindakan (Act)
+            double result = Program.EvaluateExpression(expression);
+        }
+    }
+}
